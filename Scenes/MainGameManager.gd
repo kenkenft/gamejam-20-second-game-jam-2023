66f57126900ff79c247813f2e3f98522116239e8
kenkenft <71190901+kenkenft@users.noreply.github.com>
2023-11-25 17:52:22 +0000

    Add input check and new input action mappings

--- Scenes/MainGameManager.gd
+++ Scenes/MainGameManager.gd
@@ -2,9 +2,17 @@ extends Node2D
 
 export var fruitSounds = [] #Index 0-6 are "Correct" fruit sounds, index 7 is "Incorrect" sound 
 
+var acceptedInputActions = ["gameplay_sort_easy", "gameplay_sort_medium", "gameplay_sort_hard"] 
+var selectedDifficulty = 2
+var inputToFruitId = {KEY_L: 0, KEY_S: 1, KEY_F: 2, KEY_D: 3, KEY_K: 4, KEY_SPACE: 5, KEY_J: 6 }
+var CurrentFruitObject
+var isPlaying = false
+var isCheckingInput = false
+var random = RandomNumberGenerator.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("CurrentFruit").SetNewFruit(1)
+	SetUp()
 
 #ToDo Set up table/dictionary for fruit ids and keyboard inputs
 #ToDo Check keyboard input against current fruit
@@ -12,10 +20,25 @@ func _ready():
 	#ToDo Reset multiplier and play incorrect tone on incorrect button input
 #ToDo Generate next fruit (based on chord progression and available fruits) 
 
-# ToDo: Set up game by difficulty
-func _setUp():
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	#pass
+	#if(isPlaying && !isCheckingInput)
+
+func _input(event):
+	if (isPlaying && !isCheckingInput && event.is_action_pressed(acceptedInputActions[selectedDifficulty])):
+		#isCheckingInput = true
+		
+		print(event.as_text())
+		if(inputToFruitId[event.scancode] == get_node("CurrentFruit").currentFruit):
+			CurrentFruitObject.SetNewFruit(random.randi_range(0,6))
+
+# ToDo: Set up game by difficulty
+func SetUp():
+	isPlaying = true
+	isCheckingInput = false
+	CurrentFruitObject = get_node("CurrentFruit")
+	CurrentFruitObject.SetNewFruit(1)
+
+func CheckInputCorrect(key):
+	pass
