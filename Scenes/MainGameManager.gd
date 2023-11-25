5b9bb260ae05b423f244dd8649b369a09a6c48b8
kenkenft <71190901+kenkenft@users.noreply.github.com>
2023-11-25 21:15:36 +0000

    Add methods and functions for modifying multiplier and score

--- Scenes/MainGameManager.gd
+++ Scenes/MainGameManager.gd
@@ -3,6 +3,8 @@ extends Node2D
 export var fruitSounds = [] #Index 0-6 are "Correct" fruit sounds, index 7 is "Incorrect" sound 
 
 var acceptedInputActions = ["gameplay_sort_easy", "gameplay_sort_medium", "gameplay_sort_hard"] 
+var multiplierLimits = [4, 8, 12]
+var allowedFruits = [3, 5, 7]
 var selectedDifficulty = 2
 var inputToFruitId = {KEY_L: 0, KEY_S: 1, KEY_F: 2, KEY_D: 3, KEY_K: 4, KEY_SPACE: 5, KEY_J: 6 }
 var CurrentFruitObject
@@ -11,42 +13,58 @@ var isCheckingInput = false
 var random = RandomNumberGenerator.new()
 var audioStreamPlayer
 
+var playerUI
+
+var currentScore = 0
+var currentMultiplier = 1
+var multiplierMax
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SetUp()
 
-#ToDo Set up table/dictionary for fruit ids and keyboard inputs
-#ToDo Check keyboard input against current fruit
-	#ToDo Increase score and multiplier, and play corresponding fruit sound on correct button input
-	#ToDo Reset multiplier and play incorrect tone on incorrect button input
+#ToDo Function that is called to set selectedDifficulty
+#ToDo Increase score and multiplier on correct button input
+#ToDo Reset multiplier on incorrect button input
 #ToDo Generate next fruit (based on chord progression and available fruits) 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
-	#if(isPlaying && !isCheckingInput)
 
 func _input(event):
 	if (isPlaying && !isCheckingInput && event.is_action_pressed(acceptedInputActions[selectedDifficulty])):
 		isCheckingInput = true
-		
-		print(event.as_text())
-		if(inputToFruitId[event.scancode] == get_node("CurrentFruit").currentFruit):
-			audioStreamPlayer.stream = fruitSounds[get_node("CurrentFruit").currentFruit]
-			CurrentFruitObject.SetNewFruit(random.randi_range(0,6))
-		else:
-			audioStreamPlayer.stream = fruitSounds[7]
-			
-		audioStreamPlayer.play()
+		CompareKeyToFruit(event.scancode)
 		isCheckingInput = false
 
 # ToDo: Set up game by difficulty
 func SetUp():
 	audioStreamPlayer = get_node("AudioStreamPlayer")
+	playerUI = get_node("PlayerUI")
+	multiplierMax = multiplierLimits[selectedDifficulty]
+	currentScore = 0
 	isPlaying = true
 	isCheckingInput = false
 	CurrentFruitObject = get_node("CurrentFruit")
 	CurrentFruitObject.SetNewFruit(1)
 
-func CheckInputCorrect(key):
-	pass
+func CompareKeyToFruit(key):
+	#print(key)
+	var currentFruit = get_node("CurrentFruit").currentFruit
+	if(inputToFruitId[key] == currentFruit):
+		SetUpCorrectFruit(currentFruit)
+	else:
+		SetUpInCorrectFruit()
+	audioStreamPlayer.play()
+
+func SetUpCorrectFruit(fruitID):
+	currentScore += 100 * currentMultiplier
+	
+	if(currentMultiplier < multiplierMax):
+		currentMultiplier += 1
+	#ToDo Update PlayerUI.Score and Multiplier
+	audioStreamPlayer.stream = fruitSounds[fruitID]
+	CurrentFruitObject.SetNewFruit(random.randi_range(0,6))
+
+func SetUpInCorrectFruit():
+	currentMultiplier = 1
+	audioStreamPlayer.stream = fruitSounds[7]
+	#ToDo create yield/coroutine function that waits for 0.5 seconds
