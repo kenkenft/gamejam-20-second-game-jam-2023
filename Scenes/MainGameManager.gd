188264778d448b76d1dd1ea6223a56de1eed479b
kenkenft <71190901+kenkenft@users.noreply.github.com>
2023-11-26 22:36:22 +0000

    Add new action input and crate layout that feels less awkward to use

--- Scenes/MainGameManager.gd
+++ Scenes/MainGameManager.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 export var fruitSounds = [] #Index 0-6 are "Correct" fruit sounds, index 7 is "Incorrect" sound 
 
-var acceptedInputActions = ["gameplay_sort_easy", "gameplay_sort_medium", "gameplay_sort_hard"] 
+#var acceptedInputActions = ["gameplay_sort_easy", "gameplay_sort_medium", "gameplay_sort_hard"] # Linear, in a row keyset. Felt awkward to use
+#var acceptedInputActions = ["gameplay_sort_easy_v2", "gameplay_sort_medium_v2", "gameplay_sort_hard_v2"] # More circular key set. Still awkward
+var acceptedInputActions = ["gameplay_sort_easy_v3", "gameplay_sort_medium_v3", "gameplay_sort_hard_v3"] # WASD and IJKL scheme. 
 var multiplierLimits = [4, 8, 12]
 var allowedFruits = [3, 5, 7]
 var selectedDifficulty = 2
-var inputToFruitId = {KEY_L: 0, KEY_S: 1, KEY_F: 2, KEY_D: 3, KEY_K: 4, KEY_SPACE: 5, KEY_J: 6 }
+#var inputToFruitId = {KEY_L: 0, KEY_S: 1, KEY_F: 2, KEY_D: 3, KEY_K: 4, KEY_SPACE: 5, KEY_J: 6 } # Linear, in a row keyset. Felt awkward to use
+#var inputToFruitId = {KEY_H: 0, KEY_F: 1, KEY_D: 2, KEY_E: 3, KEY_U: 4, KEY_SPACE: 5, KEY_J: 6 } # More circular key set. Still awkward
+var inputToFruitId = {KEY_L: 0, KEY_A: 1, KEY_D: 2, KEY_W: 3, KEY_I: 4, KEY_SPACE: 5, KEY_J: 6 }# WASD and IJKL scheme.
 var CurrentFruitObject
 var isPlaying = false
 var isCheckingInput = false
