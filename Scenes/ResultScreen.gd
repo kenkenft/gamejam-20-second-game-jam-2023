65ccf297f09bea651316ba32a8b715e78ea49f2c
kenkenft <71190901+kenkenft@users.noreply.github.com>
2023-11-28 17:06:19 +0000

    Add checks that only give bonus scores when the player has attempted to play.

--- Scenes/ResultScreen.gd
+++ Scenes/ResultScreen.gd
@@ -20,6 +20,8 @@ var idToName = {0: "Apple", 1: "Banana", 2: "Cherry", 3: "Durian", 4: "Egusi", 5
 
 func SetUp():
 	fruits.clear()
+	for i in range(BonusText.size()):
+		get_node(BonusText[i]).visible = false
 
 
 func PlaySong():
@@ -38,7 +40,7 @@ func _on_PlayerData_UpdateResultScreen(resultData, sortedFruits):
 	fruits.append_array(sortedFruits)	
 	difficulty = resultData[4]
 	finalScore = resultData[0]
-	
+	var finalScoreMessage = ""
 	PlaySong()
 	get_node(DynamicText[0]).bbcode_text = "[b]{0} points[/b]".format([resultData[0]])	# Initial score
 	SetBonusCombo(resultData[1])
@@ -63,37 +65,45 @@ func SetBonusCombo(comboLength):
 	var comboBonus = comboLength * 50
 	finalScore += comboBonus
 	get_node(DynamicText[1]).bbcode_text = "[b]{0}[/b]".format([comboLength])	# Longest combo
-	if(comboLength > 1):
-		#Initially hide bonus text, then make it visible
+	get_node(BonusText[0]).visible = true
+	if(comboLength >= 1):
 		get_node(BonusText[0]).bbcode_text = "[b][color=#18EE00]+{0}[/color][/b] points!".format([comboBonus])
+	else:
+		get_node(BonusText[0]).bbcode_text = "[b][color=#EF0000]You did nothing![/color][/b]".format([comboBonus])
 	get_node(DynamicText[4]).bbcode_text = "[b]{0}[/b]".format([finalScore]) # Final score after bonuses/penalties
 
 func SetBonusMultiplier(maxMultiplierAchieved):
 	var bonus = (maxMultiplierAchieved) * 100
-	finalScore += bonus
 	
 	get_node(DynamicText[2]).bbcode_text = "[b]{0}[/b]".format([maxMultiplierAchieved])	# Highest multiplier
+	get_node(BonusText[1]).visible = true
 	if(maxMultiplierAchieved > 1):
-		#Initially hide bonus text, then make it visible
+		finalScore += bonus
 		get_node(BonusText[1]).bbcode_text = "[b][color=#18EE00]+{0}[/color][/b] points!".format([bonus])
+	else:
+		get_node(BonusText[1]).bbcode_text = "[b][color=#EF0000]No bonus![/color][/b]".format([bonus])
 	get_node(DynamicText[4]).bbcode_text = "[b]{0}[/b]".format([finalScore]) # Final score after bonuses/penalties
 
 func SetBonusMistakes(mistakesMade, multiplierBonus):
 	var modifier = 0
 	var bonusText = ""
-	if(mistakesMade == 0):
+	if(mistakesMade == 0 && fruits.empty()):
+		bonusText = "[b][color=#EF0000]Didn't even try![/color][/b]"
+	elif(mistakesMade == 0):
 		modifier = multiplierBonus * 100
 		bonusText = "[b][color=#18EE00]+{0}[/color][/b] points!"
 	else:
 		modifier = mistakesMade * -100
-		bonusText = "[b][color=#EF0000]-{0}[/color][/b] points!"
+		bonusText = "[b][color=#EF0000]{0}[/color][/b] points!"
 		
 	finalScore += modifier
+	
 	get_node(DynamicText[3]).bbcode_text = "[b]{0}[/b]".format([mistakesMade]) # Mistakes
-	#Initially hide bonus text, then make it visible
+	get_node(BonusText[2]).visible = true
 	get_node(BonusText[2]).bbcode_text = bonusText.format([modifier])
 	get_node(DynamicText[4]).bbcode_text = "[b]{0}[/b]".format([finalScore]) # Final score after bonuses/penalties
 
 
 func _on_PlayerData_IndicateNewestHighScore():
+	get_node(BonusText[3]).visible = true
 	get_node(BonusText[3]).bbcode_text = "[b][color=#18EE00]New High Score![/color][/b]"
